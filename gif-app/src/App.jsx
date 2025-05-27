e47b2579b40013dd1ef4b9393c3cc603d82cf78d
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-05-27 23:04:42 +0530

    half style done

--- gif-app/src/App.jsx
+++ gif-app/src/App.jsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import RandomGif from "./components/RandomGif"
+import SearchGif from "./components/SearchGif"
+import bgImg from "./assets/bg_gif.png"
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-full h-screen flex flex-col bg-cover bg-center" style={{ backgroundImage: `url(${bgImg})`}}>
+      <div className="p-2 border border-slate-600 rounded-2xl mt-4 mx-6 bg-white">
+        <h1 className="text-[18px] font-semibold text-center">Random GIFs</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="flex flex-col items-center mt-3">
+        <RandomGif/>
+        <SearchGif/>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
