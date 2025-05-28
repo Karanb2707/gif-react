1a74cca81b6c1d34b405b433633581047defbdfe
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-05-29 01:20:04 +0530

    Done random gif

--- gif-app/src/components/RandomGif.jsx
+++ gif-app/src/components/RandomGif.jsx
@@ -1,20 +1,32 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+
+const API_KEY = import.meta.env.VITE_GIPHY_API_KEY;
 
 const RandomGif = () => {
 
     const [gif, setGif] = useState('');
 
-    function clickHandler() {
+    async function fetchData() {
+        const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`
+        const {data} = await axios.get(url);
+        const imgSource = data.data.images.downsized_large.url;
+        setGif(imgSource);        
+    }
 
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    function clickHandler() {
+        fetchData();
     }
 
     return (
         <div className='w-1/2 h-[400px] flex flex-col items-center justify-evenly gap-2 p-2 border-2 rounded-2xl 
             bg-yellow-200'>
             <h1 className='font-semibold underline'>Random GIF</h1>
-            <div className='w-[400px] h-[290px] border rounded-md'>
-                <img src={gif} />
-            </div>
+            <img src={gif} className='w-[420px] h-[280px] mx-auto mt-2 rounded-md'/>
             <button onClick={clickHandler} className='py-2 px-10 border rounded-2xl bg-red-600 text-white hover:bg-red-700 font-semibold'>
                 Generate
             </button>
