724f11d72cc45812140e6a60b2dde4efa03a5d6f
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-05-28 22:48:08 +0530

    style changes

--- gif-app/src/App.jsx
+++ gif-app/src/App.jsx
@@ -5,11 +5,11 @@ import bgImg from "./assets/bg_gif.png"
 
 const App = () => {
   return (
-    <div className="w-full h-screen flex flex-col bg-cover bg-center" style={{ backgroundImage: `url(${bgImg})`}}>
+    <div className="w-full h-full flex flex-col bg-center" style={{ backgroundImage: `url(${bgImg})`}}>
       <div className="p-2 border border-slate-600 rounded-2xl mt-4 mx-6 bg-white">
         <h1 className="text-[18px] font-semibold text-center">Random GIFs</h1>
       </div>
-      <div className="flex flex-col items-center mt-3">
+      <div className="flex flex-col items-center mt-4  gap-6 mb-4">
         <RandomGif/>
         <SearchGif/>
       </div>
--- gif-app/src/components/RandomGif.jsx
+++ gif-app/src/components/RandomGif.jsx
@@ -12,7 +12,7 @@ const RandomGif = () => {
         <div className='w-1/2 h-[400px] flex flex-col items-center justify-evenly gap-2 p-2 border-2 rounded-2xl 
             bg-yellow-200'>
             <h1 className='font-semibold underline'>Random GIF</h1>
-            <div className='w-[380px] h-[290px] border rounded-md'>
+            <div className='w-[400px] h-[290px] border rounded-md'>
                 <img src={gif} />
             </div>
             <button onClick={clickHandler} className='py-2 px-10 border rounded-2xl bg-red-600 text-white hover:bg-red-700 font-semibold'>
