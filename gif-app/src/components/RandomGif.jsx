5818744f1db46b337fb00d11697d6c9c96c99759
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-05-30 00:25:20 +0530

    Loader added

--- gif-app/src/components/RandomGif.jsx
+++ gif-app/src/components/RandomGif.jsx
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
+import Spinner from './Spinner';
 
 const API_KEY = import.meta.env.VITE_GIPHY_API_KEY;
 
 const RandomGif = () => {
 
     const [gif, setGif] = useState('');
+    const [loading, setLoading] = useState(false);
 
     async function fetchData() {
+        setLoading(true);
         const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`
         const {data} = await axios.get(url);
         const imgSource = data.data.images.downsized_large.url;
-        setGif(imgSource);     
+        setGif(imgSource);
+        setLoading(false);
     }
 
     useEffect(() => {
@@ -26,7 +30,9 @@ const RandomGif = () => {
         <div className='w-1/2 h-[400px] flex flex-col items-center justify-evenly gap-2 p-2 border-2 rounded-2xl 
             bg-yellow-200'>
             <h1 className='font-semibold underline'>Random GIF</h1>
-            <img src={gif} className='w-[420px] h-[280px] mx-auto mt-2 rounded-md'/>
+            {
+                loading ? <Spinner/> : <img src={gif} className='w-[420px] h-[280px] mx-auto mt-2 rounded-md'/>
+            }
             <button onClick={clickHandler} className='py-2 px-10 border rounded-2xl bg-red-600 text-white hover:bg-red-700 font-semibold'>
                 Generate
             </button>
---
+++ gif-app/src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Spinner = () => {
+  return (
+    <div className='border-4 border-black border-t-slate-300  h-14 w-14 rounded-full animate-spin'></div>
+  )
+}
+
+export default Spinner
